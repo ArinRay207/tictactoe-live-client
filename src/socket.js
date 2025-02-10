3f9240f1ad1205a2aa1d5d4973339a7726793026
Arin Ray <arinrayrusho@gmail.com>
2025-02-10 12:38:42 +0530

    minor changes

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://tic-tac-toe-live-server-pzly445qf-arinray207s-projects.vercel.app/';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://tic-tac-toe-live-server.vercel.app/';
 
 export const socket = io(URL);
